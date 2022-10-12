9dc3f4cb0541b6b8e7a46211296338dba4b0cddf
alevelmol <alevelmol@alum.us.es>
2022-10-12 18:28:28 +0200

    Proyecto casi finalizado

--- src/peliculas_test.py
+++ src/peliculas_test.py
@@ -5,7 +5,12 @@ from peliculas import lee_datos
 from peliculas2 import mostrar_numerado
 
 
-if __name__ == "__main__":
+def test_peliculas_leidas():
     PELIS = lee_datos(".\data\movie_dataset.csv")
-    print(PELIS)
+    numerototal = len(PELIS)
+    print(f"Se han leido {numerototal} películas")
     mostrar_numerado(PELIS)
+
+
+if __name__ == "__main__":
+    test_peliculas_leidas()
