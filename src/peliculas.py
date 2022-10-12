4a0f02b413205991d67afe3fd824e137eff7569f
alevelmol <alevelmol@alum.us.es>
2022-10-12 18:28:50 +0200

    Proyecto casi acabao

--- src/peliculas.py
+++ src/peliculas.py
@@ -1,12 +1,12 @@
 from ast import parse
 from calendar import month
 from collections import namedtuple
-import datetime
+from datetime import datetime
 import csv
 
 from peliculas2 import parse_bool
 
-pelis = namedtuple("Lista", "day, month, year, duration, title, subject, actor, actress, director, popularity, awards, rate")
+pelis = namedtuple("Lista", "day, month, year, fecha, duration, title, subject, actor, actress, director, popularity, awards, rate")
 
 def lee_datos(fichero):
     peliculas = []
@@ -14,14 +14,13 @@ def lee_datos(fichero):
         lector = csv.reader(f, delimiter= ";")
         next (lector)
         for day, month, year, duration, title, subject, actor, actress, director, popularity, awards, rate in lector:
-            day = int(day)
-            month = int(month)
-            year = int(year)
+            fecha = day +"/"+ month +"/"+ year
+            fecha = datetime.strptime(fecha , "%d/%m/%Y").date()
             duration = int(duration)
             popularity = int(popularity)
             awards = parse_bool(awards)
             rate = rate.replace("," , ".")
             rate = float(rate)
-            peliculas.append(pelis(fecha ,duration, title, subject, actor, actress, director, popularity, awards, rate))
+            peliculas.append(pelis(day,month ,year, fecha ,duration, title, subject, actor, actress, director, popularity, awards, rate))
 
     return peliculas
